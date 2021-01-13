f5366e8cd766e43de1272e5ebdafbcf8e5903a70
Alfonzo <75266027+Alfonzo-dll@users.noreply.github.com>
2021-01-13 09:37:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-const discord = require("discord.js");
-const cordshandler = require("cords-handler");
-const db = require("wio.db")
-const chclient = new cordshandler.Client()
-const client = new discord.Client();
+const Discord = require('discord.js');
 
-let eventhandler = new cordshandler.EventHandler(client, chclient);
-let cmdhandler = new cordshandler.CommandHandler(client, chclient);
+const client = new Discord.Client();
 
-eventhandler.setEventFolder("./events");
-eventhandler.loader();
+const { token, default_prefix } = require('./config.json');
 
-cmdhandler.setCommandFolder("./commands")
-cmdhandler.setPrefix("?");
-cmdhandler.loader();
+const db = require('quick.db');
 
-client.login("token");
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+const config = require('./config.json');
+client.config = config;
+
+ 
+client.commands= new Discord.Collection();
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.on('ready', () => {
+    console.log('Bot is online!');
+});
+ 
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    
+    let prefix = await db.get(`prefix_${message.guild.id}`)
+    if(prefix === null) prefix = default_prefix;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+ 
+client.login(token);
